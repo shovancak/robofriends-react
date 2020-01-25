2f303244a4b11908a1e9d3bdb5687f983805451e
shovancak <s.hovancak@gmail.com>
2020-01-25 16:02:59 +0100

    Updates

---
+++ precache-manifest.87a58519e820171f8b06f52f256abd26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfdf6dcae304610b2f8b41e46f8df8c",
+    "url": "/robofriends-react/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends-react/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "81c9250faaa6a3d40f8e",
+    "url": "/robofriends-react/static/css/main.f8179c0e.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends-react/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "81c9250faaa6a3d40f8e",
+    "url": "/robofriends-react/static/js/main.0eb8367b.chunk.js"
+  },
+  {
+    "revision": "433f736b572bf9e5d91f",
+    "url": "/robofriends-react/static/js/runtime-main.6be1e674.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
